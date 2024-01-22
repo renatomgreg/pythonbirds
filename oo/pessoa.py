fbfa61908778d611661c2e3745aa9decd138d78b
renatomgreg <renatomgregorio@gmail.com>
2024-01-21 21:02:41 -0300

    atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,14 @@
 class Pessoa:
-    pass
+    def __init__(self, *filhos, nome, idade):
+        self.idade=idade
+        self.nome=nome
+        self.filhos=list(filhos)
+
+if __name__ == '__main__':
+    riquelme=Pessoa(nome='Riquelme',idade=10)
+    renato=Pessoa(riquelme,nome='Renato', idade= 35)
+
+    print(renato.nome)
+    print(renato.idade)
+    for filho in renato.filhos:
+        print(filho.nome)
